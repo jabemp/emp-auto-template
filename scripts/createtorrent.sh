8e1d1e74093b3f98f550aaa9f29ad3dd50dee88c
jabemp <40218184+jabemp@users.noreply.github.com>
2018-06-13 18:25:36 +0200

    Create createtorrent.sh

---
+++ scripts/createtorrent.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function createTorrent() {
+	local inputfile="$1"
+	local outputfolder="$2"
+	local announceurl="$3"
+	if [ ! -e "$inputfile" ] || [ ! -d "$outputfolder" ]; then
+		echo "invalid input file and/or output folder"
+		return 1
+	fi
+	if [[ ! "$announceurl" =~ ^http.* ]]; then
+		echo "invalid announce url, must start with http"
+    	return 1
+	fi
+	local filename=$(basename "$1")
+	
+	if [ -f "$inputfile" ]; then
+		filenamebase="${filename%.*}"
+	else
+		filenamebase="${filename}"
+	fi
+	
+	torrentfullpath="${outputfolder}/${filenamebase}.torrent"
+	local params="-P -c \"\" -f -o ${torrentfullpath} ${inputfile} ${announceurl}"
+	local cmd="python3 $py3ct $params"
+	#echo "PY3CT command: $cmd"
+	local cmdrun=$(eval $cmd)
+	echo "$torrentfullpath"
+}
