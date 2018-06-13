e491a33ed435b04975f1996a98b4a70edbfc55fa
jabemp <40218184+jabemp@users.noreply.github.com>
2018-06-13 19:45:23 +0200

    Create customconfig.sh

---
+++ scripts/customconfig.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ "${network}" != "" ]]; then
+	networkconfig="${scriptsfolder}/${network,,}.cfg"
+	writelog "Checking network specific config..."
+	if [ -f "${networkconfig}" ]; then
+		writelog "Loading network specific config from '${networkconfig}'"
+		echo "Loading network specific config from '${networkconfig}'"
+		. "${networkconfig}"
+	else
+		writelog "No network config file found (this is not an error)"
+	fi
+fi
+
+siteconfig="${scriptsfolder}/${releasesite,,}.cfg"
+writelog "Checking site specific config..."
+if [ -f "${siteconfig}" ]; then
+	writelog "Loading site specific config from '${siteconfig}'"
+	echo "Loading site specific config from '${siteconfig}'"
+	. "${siteconfig}"
+else
+	writelog "No site config file found (this is not an error)"
+fi
