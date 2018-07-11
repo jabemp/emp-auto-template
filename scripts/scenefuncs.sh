1885adf8ca7565c26f5a7a53a71fd71dc980cd97
jabemp <40218184+jabemp@users.noreply.github.com>
2018-07-11 07:09:25 +0200

    Update scenefuncs.sh

--- scripts/scenefuncs.sh
+++ scripts/scenefuncs.sh
@@ -25,7 +25,7 @@ function getTitleCast() {
 	local val=""
 	#if scene has an official title, remove it from the releasename (and assume we are left with the female cast)
 	if [[ "${temptitle}" != "" ]]; then
-		temptitle=$(echo "${temptitle}" | sed -r 's/(:|!|,)//g' | sed -r 's/\&/And/g' | sed -r 's/ - / /g' | sed -r "s/'//g")
+		temptitle=$(echo "${temptitle}" | sed -r 's/(\.|:|!|,)//g' | sed -r 's/\&/And/g' | sed -r 's/ - / /g' | sed -r "s/'//g")
 		val=$(echo "${releasecasttitle}" | sed -r "s/${temptitle}//gi" | sed -r 's/ and / \& /gi' | sed -r 's/\s+$//g')
 	else
 		#assume the data in the release name is just female cast
